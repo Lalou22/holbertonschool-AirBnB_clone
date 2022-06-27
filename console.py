d9b7a263c080ddf358ab4db4be9479dfbee72158
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-06-27 12:38:01 -0500

    Task 6: Simple console 0.0.1

---
+++ console.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Entry point for the command interpreter of the HBNB application
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    Class for the Holberton Clone AirBnB proyect.
+    """
+    prompt = '(hbnb) '
+
+    def do_EOF(self, line):
+        """
+        Typing the command 'EOF' will cleanly exit the console session
+        """
+        return True
+
+    def do_quit(self, line):
+        """
+        Typing 'quit' will cleanly exit the console session
+        """
+        return True
+
+    def emptyline(line):
+        """
+        Empty lines will go to the next input loop
+        """
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
